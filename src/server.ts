57b209fa65503edeb9d6743bfe25932de604309f
Oluwatooni Adebiyi <53844337+aoluwatooni@users.noreply.github.com>
2022-10-19 12:44:30 +0100

    Added filter image

--- src/server.ts
+++ src/server.ts
@@ -57,5 +57,5 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
-  } );
+  } ); 
 })
